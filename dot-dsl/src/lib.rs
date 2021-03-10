4a28c079b52cfce348b8cb937fe8dd96b967d54b
Jose Martins <josemartins90@gmail.com>
2021-03-10 22:48:06 +0000

    dot-dsl: passing tests

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,25 +1,111 @@
 pub mod graph {
+    use std::collections::HashMap;
+    pub type Attrs = HashMap<String, String>;
+    use crate::graph::graph_items::{edge::Edge, node::Node};
+
     pub struct Graph {
-        nodes: Vec<graph_items::Node>,
-        edges: Vec<graph_items::Edge>
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: Attrs,
+    }
 
     pub mod graph_items {
-        pub struct Node;
-        pub struct Edge;
 
-        impl Node {
-            pub fn new() -> Self {
+        pub mod node {
+            use super::super::Attrs;
+            use std::collections::HashMap;
+
+            #[derive(Debug, PartialEq, Clone)]
+            pub struct Node {
+                pub name: String,
+                pub attrs: Attrs,
+            }
+
+            impl Node {
+                pub fn new(name: &str) -> Self {
+                    Node {
+                        name: String::from(name),
+                        attrs: HashMap::new(),
+                    }
+                }
+
+                pub fn with_attrs(self, attrs: &[(&str,&str)]) -> Self{
+                    Self {
+                        attrs: attrs
+                            .iter()
+                            .map(|(a,n)| (a.to_string(), n.to_string()))
+                            .collect(),
+                        ..self
+                    }
+                }
+                
+                pub fn get_attr(&self, attr: &str) -> Option<&str> {
+                    self.attrs.get(attr).map(|s| &s[..])
+                }
+            }
+        }
+
+        pub mod edge {
+            use super::super::Attrs;
+            use std::collections::HashMap;
+
+            #[derive(Debug, PartialEq, Clone)]
+            pub struct Edge {
+                pub attrs: Attrs,
+            }
+
+            impl Edge {
+                pub fn new(_a: &str, _b: &str) -> Self {
+                    Edge {
+                        attrs: HashMap::new(),
+                    }
+                }
+                pub fn with_attrs(self, attrs: &[(&str,&str)]) -> Self{
+                    Self {
+                        attrs: attrs
+                            .iter()
+                            .map(|(a,n)| (a.to_string(), n.to_string()))
+                            .collect(),
+                        ..self
+                    }
+                }
+ 
             }
         }
     }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph {
+                nodes: Vec::new(),
+                edges: Vec::new(),
+                attrs: HashMap::new(),
+            }
         }
-        pub fn is_empty(&self) -> bool {
+        pub fn with_nodes(self, nodes: &[Node]) -> Self {
+            Graph {
+                nodes: Vec::from(nodes), 
+                ..self
+            }
         }
-        pub fn with_nodes(self, nodes: Vec<Node>) -> Self  {
+        pub fn with_edges(self, edges: &[Edge]) -> Self {
+            Graph {
+                edges: Vec::from(edges),
+                ..self
+            }
+        }
+        pub fn with_attrs(self, attrs: &[(&str,&str)]) -> Self{
+            Self {
+                attrs: attrs
+                    .iter()
+                    .map(|(a,n)| (a.to_string(), n.to_string()))
+                    .collect(),
+                ..self
+            }
+        }
+ 
+        pub fn get_node(&self, name: &str) -> Option<&Node> {
+            self.nodes.iter().find(|n| n.name == name)
         }
     }
 }

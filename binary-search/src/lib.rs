99a4d3f2bb882c5b19e9e8437096a7118aeea66b
Jose Martins <josemartins90@gmail.com>
2021-05-04 19:56:13 +0100

    binary-search: improve style based on another solution

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,21 +1,15 @@
+use std::cmp::Ordering;
+
 pub fn find<T, U>(array: U, key: T) -> Option<usize>
 where
-    T: PartialOrd,
+    T: Ord,
     U: AsRef<[T]>,
 {
     let array = array.as_ref();
-
-    if array.is_empty() {
-        return None;
-    }
-
     let middle = array.len() / 2;
-
-    if array[middle] == key {
-        Some(middle)
-    } else if array[middle] > key {
-        find(&array[..middle], key)
-    } else {
-        find(&array[middle + 1..], key).map(|i| i + middle + 1)
+    match key.cmp(array.get(middle)?) {
+        Ordering::Equal => Some(middle),
+        Ordering::Less => find(&array[..middle], key),
+        Ordering::Greater => find(&array[middle + 1..], key).map(|i| i + middle + 1),
     }
 }

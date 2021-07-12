2a1c653dcf4491b59a15feb8bb9aaf66ca2767ac
Jose Martins <josemartins90@gmail.com>
2021-07-12 18:37:45 +0100

    palindrome-products solved

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,49 +1,50 @@
 #![feature(map_first_last)]
 
-use std::collections::BTreeMap;
+use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
-    factors: Vec<(u64, u64)>,
+    value: u64,
+    factors: HashSet<(u64, u64)>,
 }
 
 impl Palindrome {
     pub fn new(a: u64, b: u64) -> Palindrome {
         Self {
-            factors: vec![(a, b)],
+            value: a * b,
+            factors: vec![(a,b)].into_iter().collect::<HashSet<_>>(),
         }
     }
 
     pub fn value(&self) -> u64 {
-        let (a, b) = self.factors[0];
-        a * b
+        self.value
     }
 
     pub fn insert(&mut self, a: u64, b: u64) {
-        self.factors.push((a, b));
+        self.factors.insert((a, b));
     }
 }
 
-fn is_palindrome(n: u64) -> bool {
+fn is_palindrome(n: &u64) -> bool {
     let n = n.to_string();
     let rev = n.chars().rev().collect::<String>();
     n == rev
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    let mut palindromes = BTreeMap::<u64, Palindrome>::new();
-    for (a, b) in (min..=max)
-        .flat_map(|a| (a..=max).map(move |b| (a, b)))
-        .filter(|(a, b)| is_palindrome(a * b))
-    {
-        if let Some(p) = palindromes.get_mut(&(a * b)) {
-            p.insert(a, b);
-        } else {
-            palindromes.insert(a * b, Palindrome::new(a, b));
+    let mut products = (min..=max)
+        .flat_map(|a| (a..=max).map(move |b| (a*b, (a, b)))).collect::<Vec<_>>();
+    products.sort_by_key(|(n, _)| *n);
+    let min_palindrome = products.iter().filter(|(n,_)| is_palindrome(n)).next()?;
+    let max_palindrome = products.iter().rev().filter(|(n,_)| is_palindrome(n)).next()?; 
+    let mut min_palindrome = Palindrome::new(min_palindrome.1.0, min_palindrome.1.1); 
+    let mut max_palindrome = Palindrome::new(max_palindrome.1.0, max_palindrome.1.1); 
+    for (n, (a,b)) in products {
+        if n == min_palindrome.value() {
+            min_palindrome.insert(a, b);
+        } else if n == max_palindrome.value() {
+            max_palindrome.insert(a, b);
         }
     }
-    match (palindromes.pop_first(), palindromes.pop_last()) {
-        (Some((_, x)), Some((_, y))) => Some((x, y)),
-        _ => None,
-    }
+    Some((min_palindrome, max_palindrome))
 }
